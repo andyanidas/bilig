e20fdf4e9d8b553ee8cba05a17fd50030773e0f7
Lkhagva-erdene <anidas.com@gmail.com>
2021-03-14 22:07:25 +0300

    Add files via upload

---
+++ 8.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	string baby,mom,dodo;
+	baby = "Baby shark,";
+	mom = "Mommy shark,";
+	dodo = "doo, doo, doo, doo, doo, doo";
+	
+	cout<<baby<<dodo<<endl;
+	cout<<baby<<dodo<<endl;
+	cout<<baby<<dodo<<endl;
+	cout<<baby<<endl;
+	
+	cout<<mom<<dodo<<endl;
+	cout<<mom<<dodo<<endl;
+	cout<<mom<<dodo<<endl;
+	cout<<mom<<endl;
+		
+	
+	return 0;
+}
